ef57dd168c5e1c0c9be371ea80e1a871193d4b2f
Wugs <iniyanijoseph@gmail.com>
2025-06-04 16:43:43 -0500

    helix

--- home/helix.nix
+++ home/helix.nix
@@ -9,6 +9,81 @@
         insert = "bar";
         select = "underline";
       };
+
+      editor.file-picker.hidden = false;
+      keys.normal = {
+        space.w = ":w";
+        space.x = ":x";
+        pageup = "no_op";
+        home = "no_op";
+        end = "no_op";
+        A-space = "normal_mode";
+        A-w = "vsplit";
+        A-n = "rotate_view";
+        A-q = "wclose";
+        "#" = "toggle_comments";
+        "ret" = ["open_below" "normal_mode"];
+        esc = "no_op";
+        h = "move_char_right";
+        l = "move_char_left";
+        g.h = "goto_line_end";
+        g.l = "goto_line_start";
+      };
+
+      editor.soft-wrap = true;
+  
+      keys.select= {
+        space.w = ":w";
+        space.x = ":x";
+        pageup = "no_op";
+        home = "no_op";
+        end = "no_op";
+        A-space = "normal_mode";
+        A-w = "vsplit";
+        A-n = "rotate_view";
+        A-q = "wclose";
+        "#" = "toggle_comments";
+        "ret" = ["open_below" "normal_mode"];
+        esc = "no_op";
+        h = "move_char_right";
+        l = "move_char_left";
+        g.h = "goto_line_end";
+        g.l = "goto_line_start";
+      };
+      keys.insert = {
+        pageup = "no_op";
+        home = "no_op";
+        end = "no_op";
+        A-space = "normal_mode";
+        A-w = "vsplit";
+        A-n = "rotate_view";
+        A-q = "wclose";
+        esc = "no_op";
+      };
     };
+
+    languages = {
+      language = [{
+        name = "typst";
+        language-servers = ["tinymist"];
+      }];
+
+      language-server.tinymist = {
+        command = "tinymist";
+        config = {
+          preview = {
+            background = {
+              enabled = true;
+              args = ["--data-plane-host=127.0.0.1:23635" "--invert-colors=never" "--open"];
+            };
+            cursorIndicator = true;
+            scrollSync = true;
+          };
+          exportPdf = "onType";
+          formatterMode = "typstfmt";
+        };
+      };
+    };
+
   };
 }
--- home/typset.nix
+++ home/typset.nix
@@ -2,5 +2,7 @@
 {
   home.packages = with pkgs; [
     typst
+    tinymist
+    typstfmt
   ];
 }
