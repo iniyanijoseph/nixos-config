859dd3521f58f8119feb669d7f7b3b17d54a1d2a
Wugs <iniyanijoseph@gmail.com>
2025-06-09 14:50:00 -0500

    wallpapers

--- home/waypaper.nix
+++ home/waypaper.nix
@@ -5,9 +5,9 @@
   xdg.configFile."waypaper/config.ini".text = ''
     [Settings]
     language = en
-    folder = ~/Pictures/wallpapers/others
+    folder = ~/.wallpapers/others
     monitors = All
-    wallpaper = ~/Pictures/wallpapers/others/nixos.png
+    wallpaper = ~/.wallpapers/others/nixos.png
     backend = swww
     fill = fill
     sort = name
