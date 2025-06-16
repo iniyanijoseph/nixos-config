7b684eb6356157fc0b1fb13dc4654e863347e87d
Wugs <iniyanijoseph@gmail.com>
2025-06-16 11:46:08 -0500

    bg

--- home/hyprland/config.nix
+++ home/hyprland/config.nix
@@ -6,6 +6,7 @@ in
 {
   wayland.windowManager.hyprland = {
     settings = {
+
       exec-once = [
         "nm-applet &"
         "poweralertd &"
@@ -28,6 +29,35 @@ in
         float_switch_override_focus = 0;
         mouse_refocus = 0;
         sensitivity = 0;
+      };
+
+      decoration = {
+        rounding = 0;
+        active_opacity = 0.90;
+        inactive_opacity = 0.90;
+        fullscreen_opacity = 1.0;
+
+        blur = {
+          enabled = true;
+          size = 3;
+          passes = 2;
+          brightness = 1;
+          contrast = 1.4;
+          ignore_opacity = true;
+          noise = 0;
+          new_optimizations = true;
+          xray = true;
+        };
+
+        shadow = {
+          enabled = true;
+
+          ignore_window = true;
+          offset = "0 2";
+          range = 20;
+          render_power = 3;
+          color = "rgba(00000055)";
+        };
       };
 
       general = {
