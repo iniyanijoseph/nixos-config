222b9115efce0612141d062b339c23ec1867c1e9
Wugs <iniyanijoseph@gmail.com>
2025-10-18 10:45:39 -0500

    goofcord

--- home/thunderbird.nix
+++ home/thunderbird.nix
@@ -1,6 +1,6 @@
 {pkgs, ...}:
 {
-  home.packages=with pkgs; [discord];
+  home.packages=with pkgs; [discordo goofcord];
   programs.thunderbird = {
     enable = true;
     profiles.wug.isDefault = true;
