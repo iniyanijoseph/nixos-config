2e44f54466443bc4c27bee3519fd75458d578fba
Wugs <iniyanijoseph@gmail.com>
2025-10-29 16:16:50 -0500

    zoom and ytmdesktop

--- home/mpv.nix
+++ home/mpv.nix
@@ -1,4 +1,5 @@
 {pkgs, ...}:
 {
+  home.packages = with pkgs; [ytmdesktop];
   programs.mpv.enable = true;
 }
--- home/thunderbird.nix
+++ home/thunderbird.nix
@@ -1,6 +1,6 @@
 {pkgs, ...}:
 {
-  home.packages=with pkgs; [];
+  home.packages=with pkgs; [zoom-us];
   programs.thunderbird = {
     enable = true;
     profiles.wug.isDefault = true;
