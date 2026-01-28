d38425dc23eba0ac47cdab7efca9ce2548029cef
Wugs <iniyanijoseph@gmail.com>
2026-01-28 13:03:09 -0600

    latex and luatex

--- home/typset.nix
+++ home/typset.nix
@@ -1,9 +1,16 @@
 {pkgs, ...}:
-{
+let
+  tex = (pkgs.texlive.combine {
+    inherit (pkgs.texlive) scheme-medium
+      wrapfig amsmath ulem hyperref capt-of
+      luatex;
+  });
+in {
   home.packages = with pkgs; [
     typst
     tinymist
     typstyle
     harper
+    tex
   ];
 }
