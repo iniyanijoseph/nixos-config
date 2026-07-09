cbe44499a0f8ce92eebb8243eeff2862680edacf
Wugs <iniyanijoseph@gmail.com>
2026-07-08 22:58:07 -0500

    geary instead of this nonsense make it access keyring

--- home/mail.nix
+++ home/mail.nix
@@ -1,5 +1,13 @@
 { pkgs, ... }:
 
 {
-  home.packages = with pkgs; [geary];
+  home.packages = with pkgs; [
+    geary
+    gcr
+  ];
+
+  services.gnome-keyring.enable = true;
+
+  # Ensure the freedesktop secret service can find the user display/session env.
+  xdg.portal.enable = true;
 }
